06d29eda235bfb95b8ab7389d7119474878becae
Yasmin Cibreiros Chagas <114839561+yasminccs@users.noreply.github.com>
2024-10-02 20:32:26 -0300

    Update ContaTerminal.java
    add novas mudanças

--- ContaTerminal.java
+++ ContaTerminal.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class ContaTerminal extends dadosUsuario {
     static Scanner myObj = new Scanner(System.in);
     static dadosUsuario usuario = new dadosUsuario();
-    
+
     public static void main(String[] args){
         boolean cond = true;
 
@@ -18,29 +18,7 @@ public class ContaTerminal extends dadosUsuario {
                     accessUser();
                     break;
                 case "2":
-                    System.out.println("Informe seu nome completo: ");
-                    String nome = myObj.nextLine();
-                    System.out.println("Informe seu endereço: ");
-                    String endereco = myObj.nextLine();
-                    System.out.println("Informe seu CPF: ");
-                    int cpf = myObj.nextInt();
-                    myObj.nextLine();
-                    System.out.println("Nome: " + nome + "\n" + "Endereço: " + endereco + "\n" + "CPF: " + cpf);
-
-                    System.out.println("Confirma os dados acima?");
-                    String respd = myObj.nextLine().toUpperCase();
-                    if(respd.equals("S")){
-                        System.out.println("Dados confirmados.");
-                    } else {
-                        System.out.println("Dados não confirmados.");
-                        break;
-                    }
-
-                    Random random = new Random();
-                    long contaCorrenteGerada = (long) (random.nextDouble() * 1_000_000_0000L);
-                    int agenciaGerada = 10000 + random.nextInt(90000);
-
-                    System.out.println("Sua agência: " + agenciaGerada + "\n" + "Sua conta-corrente: " + contaCorrenteGerada);
+                    createContaUser();
                     break;
                 case "3":
                     deleteContaUser();
@@ -60,7 +38,7 @@ public class ContaTerminal extends dadosUsuario {
     }
     public static void accessUser(){
         usuario.infos();
-        System.out.println("Nome: " + usuario.getNome() + "\n" + "Agência: " + usuario.getAgencia() + "\n" + "Conta corrente: " + usuario.getContaCorrente());
+        System.out.println("Nome: " + usuario.getNome() + " | " + "Agência: " + usuario.getAgencia() + " | " + "Conta corrente: " + usuario.getContaCorrente());
         System.out.println("Confirma os dados acima?");
         String res = myObj.nextLine().toUpperCase();
         if(res.equals("S")){
@@ -71,7 +49,29 @@ public class ContaTerminal extends dadosUsuario {
     }
 
     public static void createContaUser(){
-        
+        System.out.println("Informe seu nome completo: ");
+        String nome = myObj.nextLine();
+        System.out.println("Informe seu endereço: ");
+        String endereco = myObj.nextLine();
+        System.out.println("Informe seu CPF: ");
+        int cpf = myObj.nextInt();
+        myObj.nextLine();
+        System.out.println("Nome: " + nome + " | " + "Endereço: " + endereco + " | " + "CPF: " + cpf);
+
+        System.out.println("Confirma os dados acima?");
+        String respd = myObj.nextLine().toUpperCase();
+        if(respd.equals("S")){
+            System.out.println("Dados confirmados com sucesso! A seguir, informaremos sua agência e conta corrente.");
+        } else {
+            System.out.println("Dados não confirmados.");
+            return;
+        }
+
+        Random random = new Random();
+        long contaCorrenteGerada = (long) (random.nextDouble() * 1_000_000_0000L);
+        int agenciaGerada = 10000 + random.nextInt(90000);
+
+        System.out.println("Sua agência: " + agenciaGerada + " | " + "Sua conta corrente: " + contaCorrenteGerada);
     }
 
     public static void deleteContaUser(){
